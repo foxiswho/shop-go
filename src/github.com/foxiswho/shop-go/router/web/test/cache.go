73fe93e25cd38e2d8d811a995121a23d7ac1059d
foxiswho <foxiswho@gmail.com>
2018-05-12 12:29:45 +0800

    test

---
+++ src/github.com/foxiswho/shop-go/router/web/test/cache.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"github.com/foxiswho/shop-go/router/base"
+	"github.com/foxiswho/shop-go/module/cache"
+	"time"
+	"fmt"
+)
+
+func CacheHandler(c *base.BaseContext) error {
+
+	err := cache.Client().Set("test", time.Now(), 10*time.Minute)
+	fmt.Println("err", err)
+	cache_test :=""
+	cache.Client().Get("test", &cache_test)
+	c.Set("tmpl", "web/test/cache")
+	c.Set("data", map[string]interface{}{
+		"title":      "测试 缓存",
+		"cache_test": cache_test,
+	})
+
+	return nil
+}
