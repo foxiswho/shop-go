600aa32b299cf5d9e8800682a00e151bb4902962
foxiswho <foxiswho@gmail.com>
2018-07-27 13:09:19 +0800

    增加 example 域名

--- module/conf/server.go
+++ module/conf/server.go
@@ -4,8 +4,9 @@ type server struct {
 	Graceful bool   `toml:"graceful"`
 	Addr     string `toml:"addr"`
 
-	DomainApi    string `toml:"domain_api"`
-	DomainAdmin  string `toml:"domain_admin"`
-	DomainWww    string `toml:"domain_www"`
-	DomainSocket string `toml:"domain_socket"`
+	DomainApi     string `toml:"domain_api"`
+	DomainAdmin   string `toml:"domain_admin"`
+	DomainWww     string `toml:"domain_www"`
+	DomainSocket  string `toml:"domain_socket"`
+	DomainExample string `toml:"domain_example"`
 }
