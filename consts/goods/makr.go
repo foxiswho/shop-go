4fae35d2c7f7bb6388d6249d2e719969aca6f4a1
foxiswho <foxiswho@gmail.com>
2018-07-31 16:28:10 +0800

    自定义类型

---
+++ consts/goods/makr.go
@@ -0,0 +1,6 @@
+package goods
+
+const (
+	Mark_Default= 10001	//默认商品 产品-仓库-供应商
+	Mark_Combined= 10002	//组合商品 产品-仓库-供应商
+)
---
+++ consts/goods/price.go
@@ -0,0 +1,7 @@
+package goods
+
+const (
+	//价格类别
+	Price_Type_Default =11001	//默认
+	Price_Type_Discount =11002	//优惠
+)
