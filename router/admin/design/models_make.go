204415159d544b545d56579e2f0b51407f577489
foxiswho <foxiswho@gmail.com>
2018-08-11 22:36:22 +0800

    更新路由控制器

--- router/admin/design/models_make.go
+++ router/admin/design/models_make.go
@@ -4,37 +4,26 @@ import (
 	"github.com/foxiswho/shop-go/module/context"
 	"net/http"
 	"github.com/labstack/echo"
-	"os/exec"
-	"github.com/foxiswho/shop-go/module/log"
-	"github.com/foxiswho/shop-go/module/conf"
 	"fmt"
+	"github.com/foxiswho/shop-go/module/design/models"
+	"github.com/foxiswho/shop-go/module/log"
 )
 
 func ModelsMakeHandler(c *context.BaseContext) error {
-	return c.JSON(http.StatusOK, echo.Map{
-		"code":    http.StatusBadRequest,
-		"message": "此功能 未完成",
-	})
-	//
+
+	template_file := "./template/design/make/models.go.tpl"
+	fmt.Println(template_file)
 	json:=make(map[string]interface{})
 	c.FormJson(json)
-	log.Debugf(" FORM JSON: %v", json)
-
-	command := " reverse mysql "+conf.Conf.DB.UserName+":"+conf.Conf.DB.Pwd+"@/"+conf.Conf.DB.Name+"?charset=utf8 template/design/goxorm"
-	//
-	log.Debugf("Command: xorm %v",command)
-	fmt.Println("xorm",command)
-	cmd := exec.Command("xorm", command)
-	bytes,err := cmd.Output()
-	if err != nil {
-		log.Debugf("Command Error: %v",err)
-		return c.JSON(http.StatusOK, echo.Map{
-			"code":    http.StatusBadRequest,
-			"message": " 生成失败 error:" + err.Error(),
-		})
+	log.Debugf(" input dir: %v", json)
+	path := json["dir"].(string)
+	log.Debugf(" input dir: %v", path)
+	if len(path) < 1 {
+		path = "models"
 	}
-	resp := string(bytes)
-	log.Debugf("Command RESULT: %v",resp)
+	models.MakeModels(template_file,path)
+
+
 	return c.JSON(http.StatusOK, echo.Map{
 		"code":    http.StatusOK,
 		"message": "生成成功",
--- router/admin/design/service_make.go
+++ router/admin/design/service_make.go
@@ -1,185 +1,33 @@
 package design
 
 import (
-	"github.com/foxiswho/shop-go/module/db"
-	"text/template"
-	"os"
-	"strings"
-	"github.com/foxiswho/shop-go/module/conf"
 	"net/http"
 	"github.com/foxiswho/shop-go/module/context"
 	"github.com/labstack/echo"
 	"github.com/foxiswho/shop-go/module/log"
+	"github.com/foxiswho/shop-go/module/design/service"
 )
 
 func ServiceMakeHandler(c *context.BaseContext) error {
-
-	sql := "show tables"
-	result, err := db.Db().Engine.QueryString(sql)
+	template_file := "./template/design/make/service.go.tpl"
+	json := make(map[string]interface{})
+	c.FormJson(json)
+	log.Debugf(" input dir: %v", json)
+	service_path := json["dir"].(string)
+	log.Debugf(" input dir: %v", service_path)
+	if len(service_path) < 1 {
+		service_path = "./models/crud"
+	}
+	err := service.MakeService(template_file, service_path)
 	if err != nil {
 		return c.JSON(http.StatusOK, echo.Map{
 			"code":    http.StatusBadRequest,
-			"message": "数据库错误:" + err.Error(),
+			"message": err.Error(),
 		})
 	} else {
-		template_file := "./template/design/make/service.go.tpl"
-
-		json:=make(map[string]interface{})
-		c.FormJson(json)
-		log.Debugf(" input dir: %v", json)
-		service_path := json["dir"].(string)
-		log.Debugf(" input dir: %v", service_path)
-		if len(service_path) < 1 {
-			service_path = "./models/crud"
-		}
-		field := "Tables_in_" + conf.Conf.DB.Name
-		for _, val := range result {
-			tmpl, err := template.ParseFiles(template_file)
-			data := make(map[string]interface{})
-			data["tables"] = val[field]
-			//data["tables_first"] = gonicCasedName(val[field])
-			data["tables_Camel_Case"] = LintGonicMapper.Table2Obj(val[field])
-			err = os.MkdirAll(service_path, os.ModePerm)
-			if err != nil {
-				log.Debugf("Create Directory ERROR! : %v", err)
-			}
-			log.Debugf("Create Directory OK! : %v", service_path)
-			service_file := service_path + "/" + val[field] + ".go"
-			file, err := os.OpenFile(service_file, os.O_CREATE|os.O_WRONLY, os.ModePerm)
-			if err != nil {
-				return c.JSON(http.StatusOK, echo.Map{
-					"code":    http.StatusBadRequest,
-					"message": service_file + " 目录中 不能创建文件或不能创建目录 error:" + err.Error(),
-				})
-			} else {
-				err = tmpl.Execute(file, data)
-				if err != nil {
-					log.Debugf(" Execute: %v", err)
-				}
-			}
-			//break
-		}
 		return c.JSON(http.StatusOK, echo.Map{
 			"code":    http.StatusOK,
 			"message": "生成成功",
 		})
 	}
-	return nil
-}
-
-func gonicCasedName(name string) string {
-	newstr := make([]rune, 0, len(name)+3)
-	for idx, chr := range name {
-		if isASCIIUpper(chr) && idx > 0 {
-			if !isASCIIUpper(newstr[len(newstr)-1]) {
-				newstr = append(newstr, '_')
-			}
-		}
-
-		if !isASCIIUpper(chr) && idx > 1 {
-			l := len(newstr)
-			if isASCIIUpper(newstr[l-1]) && isASCIIUpper(newstr[l-2]) {
-				newstr = append(newstr, newstr[l-1])
-				newstr[l-1] = '_'
-			}
-		}
-
-		newstr = append(newstr, chr)
-	}
-	return strings.ToLower(string(newstr))
-}
-func isASCIIUpper(r rune) bool {
-	return 'A' <= r && r <= 'Z'
-}
-
-func toASCIIUpper(r rune) rune {
-	if 'a' <= r && r <= 'z' {
-		r -= ('a' - 'A')
-	}
-	return r
-}
-
-// GonicMapper implements IMapper. It will consider initialisms when mapping names.
-// E.g. id -> ID, user -> Admin and to table names: UserID -> user_id, MyUID -> my_uid
-type GonicMapper map[string]bool
-
-func (mapper GonicMapper) Obj2Table(name string) string {
-	return gonicCasedName(name)
-}
-
-func (mapper GonicMapper) Table2Obj(name string) string {
-	newstr := make([]rune, 0)
-
-	name = strings.ToLower(name)
-	parts := strings.Split(name, "_")
-
-	for _, p := range parts {
-		_, isInitialism := mapper[strings.ToUpper(p)]
-		for i, r := range p {
-			if i == 0 || isInitialism {
-				r = toASCIIUpper(r)
-			}
-			newstr = append(newstr, r)
-		}
-	}
-
-	return string(newstr)
-}
-
-var LintGonicMapper = GonicMapper{
-	"API":   true,
-	"ASCII": true,
-	"CPU":   true,
-	"CSS":   true,
-	"DNS":   true,
-	"EOF":   true,
-	"GUID":  true,
-	"HTML":  true,
-	"HTTP":  true,
-	"HTTPS": true,
-	"ID":    true,
-	"IP":    true,
-	"JSON":  true,
-	"LHS":   true,
-	"QPS":   true,
-	"RAM":   true,
-	"RHS":   true,
-	"RPC":   true,
-	"SLA":   true,
-	"SMTP":  true,
-	"SSH":   true,
-	"TLS":   true,
-	"TTL":   true,
-	"UI":    true,
-	"UID":   true,
-	"UUID":  true,
-	"URI":   true,
-	"URL":   true,
-	"UTF8":  true,
-	"VM":    true,
-	"XML":   true,
-	"XSRF":  true,
-	"XSS":   true,
-}
-
-/**
- * 字符串首字母转化为大写 ios_bbbbbbbb -> iosBbbbbbbbb
- */
-func strFirstToUpper(str string) string {
-	temp := strings.Split(str, "_")
-	var upperStr string
-	for y := 0; y < len(temp); y++ {
-		vv := []rune(temp[y])
-		if y != 0 {
-			for i := 0; i < len(vv); i++ {
-				if i == 0 {
-					vv[i] -= 32
-					upperStr += string(vv[i]) // + string(vv[i+1])
-				} else {
-					upperStr += string(vv[i])
-				}
-			}
-		}
-	}
-	return temp[0] + upperStr
 }
