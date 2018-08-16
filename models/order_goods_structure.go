159f6de14c4b5c374b61a99895955ede0ec3de4f
foxiswho <foxiswho@gmail.com>
2018-08-16 18:48:32 +0800

    商品价格

--- models/order_goods_structure.go
+++ models/order_goods_structure.go
@@ -3,6 +3,7 @@ package models
 type OrderGoodsStructure struct {
 	Id          int     `json:"id" xorm:"not null pk autoincr INT(10)"`
 	OrderId     int     `json:"order_id" xorm:"not null default 0 comment('订单ID') index INT(10)"`
+	PriceId     int     `json:"price_id" xorm:"not null default 0 comment('价格id') INT(11)"`
 	GoodsId     int     `json:"goods_id" xorm:"not null default 0 comment('商品ID') INT(10)"`
 	ProductId   int     `json:"product_id" xorm:"not null default 0 comment('商品信息id') INT(10)"`
 	Title       string  `json:"title" xorm:"default 'NULL' comment('商品名称') VARCHAR(200)"`
@@ -19,7 +20,7 @@ type OrderGoodsStructure struct {
 	VatFee      float64 `json:"vat_fee" xorm:"not null default 0.0000 comment('增值税费') DECIMAL(28,4)"`
 	PriceTax    float64 `json:"price_tax" xorm:"not null default 0.0000 comment('总税费') DECIMAL(28,4)"`
 	Remark      string  `json:"remark" xorm:"default 'NULL' comment('备注') TEXT"`
-	PriceShop   float64 `json:"price_shop" xorm:"default 0.0000 comment('商城价') DECIMAL(28,4)"`
+	PriceShop   float64 `json:"price_shop" xorm:"not null default 0.0000 comment('商城价') DECIMAL(28,4)"`
 	CostPrice   float64 `json:"cost_price" xorm:"not null default 0.0000 comment('成本单价') DECIMAL(28,4)"`
 	CostAmount  float64 `json:"cost_amount" xorm:"not null default 0.0000 comment('成本金额') DECIMAL(28,4)"`
 	ParentId    int     `json:"parent_id" xorm:"not null default 0 comment('所属组合商品ID') INT(11)"`
